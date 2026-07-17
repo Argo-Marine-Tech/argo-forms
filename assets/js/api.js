dffe0d673be37e1be8b088608fadb38770b190d7
Argo-Marine-Tech <argo.ict2025@gmail.com>
2026-07-17 14:41:14 +1000

    Implement API object for form submission
    Added API object with submit method for form submission.

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,24 @@
-/* API JS */
+window.Argo = window.Argo || {};
+
+window.Argo.API = {
+
+    endpoint: "https://argo-digitalforms-api-c9h3ejhtdvdva2b2.australiaeast-01.azurewebsites.net/api/forms",
+
+    async submit(formData) {
+
+        const response = await fetch(this.endpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        });
+
+        if (!response.ok) {
+            throw new Error("Unable to submit form.");
+        }
+
+        return await response.json();
+    }
+
+};
